2b9812ad14a6521cb26dd3cfc0e60e44eaa2f704
Kylor Hall <kylor@kylorhall.com>
2021-09-12 17:14:27 +1200

    Add in some API interaction into the action reducer.

--- src/context/Users/reducer.ts
+++ src/context/Users/reducer.ts
@@ -1,19 +1,36 @@
+import axios from 'axios';
+
 import { AddAction, DeleteAction, UpdateAction, InitAction, User, Action } from './types'
 
 export const usersReducer = (users: User[] | undefined = [], { type, payload } : Action): User[] => {
   switch (type) {
+    // Append a User.
     case 'ADD':
-      // Append a User.
-      return [...users, (payload as AddAction['payload'])];
+      const createUser = (payload as AddAction['payload']);
 
+      // NOTE: You'd probably await this or have multiple dispatches before/after async to temporarily and finally update.
+      axios.post(`https://jsonplaceholder.typicode.com/users`, createUser);
+
+      return [...users, createUser];
+
+    // Delete the User from the state.
     case 'DELETE':
-      // Delete a User.
-      return users.filter(user => user.id !== (payload as DeleteAction['payload']).id);
+      const deleteUser = (payload as DeleteAction['payload']);
 
+      // NOTE: You'd probably await this or have multiple dispatches before/after async to temporarily and finally update.
+      axios.delete(`https://jsonplaceholder.typicode.com/users/${deleteUser.id}`);
+
+      return users.filter(user => user.id !== deleteUser.id);
+
+    // Update/Replace a User; this returns the payload in its place, does not merge.
     case 'UPDATE':
-      // Update/Replace a User; this returns the payload in its place, does not merge.
+      const updateUser = (payload as UpdateAction['payload']);
+
+      // NOTE: You'd probably await this or have multiple dispatches before/after async to temporarily and finally update.
+      axios.put(`https://jsonplaceholder.typicode.com/users/${updateUser.id}`, updateUser);
+
       return users.map(user => {
-        if (user.id === (payload as UpdateAction['payload']).id) return (payload as UpdateAction['payload']);
+        if (user.id === updateUser.id) return updateUser;
         return user;
       });
 
