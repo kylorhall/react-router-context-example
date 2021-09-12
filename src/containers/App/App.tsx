da0f5504ade4dee3e0ad25bf4b16bca0e74c5a55
Kylor Hall <kylor@kylorhall.com>
2021-09-12 16:00:12 +1200

    Lots of cleanup from bad CRA defaults.
     - Module resolution is crap out of the box.
     - Type resolution is crap out of the box.
     - `reportWebVitals` doesn't work out of the box, why is it here?
     - Missing types out of the box.
     - Bad tsconfig defaults.

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import type { VFC } from 'react';
+
+export const App: VFC<{}> = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +23,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
